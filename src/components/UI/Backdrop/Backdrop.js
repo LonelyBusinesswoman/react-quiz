481b11179408269684ecc173b7ae866e1f32086f
Lizaveta <elis.liza567@gmail.com>
2020-11-09 18:52:54 +0300

    quiz with menu but without route

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import classes from './Backdrop.module.scss'
+
+
+const Backdrop = props => {
+    return(
+        <div 
+            className={classes.Backdrop}
+            onClick={props.onClick}
+        />
+    )
+   
+}
+
+export default Backdrop
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react'
 import classes from './Layout.module.scss'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
+
 
 class Layout extends Component{
+    state = {
+        menu: false,
+    }
+    toggleNemuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+    menuCloseHandler = () =>{
+        this.setState({
+            menu: false
+        })
+    }
     render() {
         return(
             <div className={classes.Layout}>
+                <Drawer
+                    isOpen={this.state.menu}
+                    onClose={this.menuCloseHandler}
+                >
 
+                </Drawer>
+                <MenuToggle
+                    onToggle={this.toggleNemuHandler}
+                    isOpen={this.state.menu}
+                >
+
+                </MenuToggle>
                 <main className={classes['main']}>
                     {this.props.children}
                 </main>
